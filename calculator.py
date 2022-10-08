c004854dd891d4d3e041f18ca3a25fab4aa521c8
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2022-10-08 12:45:04 -0300

    Primeira versão (1.0) com modo interativo habilitado

--- calculator.py
+++ calculator.py
@@ -1,7 +1,20 @@
-from operation import operator_verification, interactive_addition
+from numpy import product
+from operation import interactive_subtraction, operator_verification, interactive_addition, interactive_multiplication, interactive_division
 
 interaction_data = input("Digite uma operação matemática: ")
 operantion_dict = operator_verification(interaction_data)
 if operantion_dict["operator"] == "+":
     sum = interactive_addition(operantion_dict["operand"])
     print("Soma = {}".format(sum))
+
+elif operantion_dict["operator"] == "-":
+    difference = interactive_subtraction(operantion_dict["operand"])
+    print("Diferença = {}".format(difference))
+
+elif operantion_dict["operator"] == "*":
+    produto = interactive_multiplication(operantion_dict["operand"])
+    print("Produto = {}".format(produto))
+
+elif operantion_dict["operator"] == "/":
+    quotient = interactive_division(operantion_dict["operand"])
+    print("Quociente = {}".format(quotient))
--- operation.py
+++ operation.py
@@ -19,3 +19,24 @@ def interactive_addition(operand):
     for x in operand:
         sum += int(x)
     return sum
+
+def interactive_subtraction(operand):
+    difference = int(operand[0])
+    operand = list(operand[1:])
+    for x in operand:
+        difference = difference - int(x)
+    return difference
+
+def interactive_multiplication(operand):
+    product = int(operand[0])
+    operand = list(operand[1:])
+    for x in operand:
+        product = product * int(x)
+    return product
+
+def interactive_division(operand):
+    quotient = int(operand[0])
+    operand = list(operand[1:])
+    for x in operand:
+        quotient = quotient / int(x)
+    return quotient
