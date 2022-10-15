2480fd3c7fc6abcdada9b0f33c5210acf2217549
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2022-10-15 10:09:59 -0300

    Modo por parâmetros ativado

--- calculator.py
+++ calculator.py
@@ -1,20 +1,56 @@
-from numpy import product
-from operation import interactive_subtraction, operator_verification, interactive_addition, interactive_multiplication, interactive_division
+#!/usr/bin/python3
 
-interaction_data = input("Digite uma operação matemática: ")
-operantion_dict = operator_verification(interaction_data)
-if operantion_dict["operator"] == "+":
-    sum = interactive_addition(operantion_dict["operand"])
-    print("Soma = {}".format(sum))
+from sys import argv
+from operation import subtraction, operator_verification, addition, multiplication, division
 
-elif operantion_dict["operator"] == "-":
-    difference = interactive_subtraction(operantion_dict["operand"])
-    print("Diferença = {}".format(difference))
+if len(argv) > 1:
+    if argv[1] == "-a":
+        operation = []
+        for x in argv[2:]:
+            operation.append(int(x))
+        sum = addition(operation)
+        print("Soma = {}".format(sum))
 
-elif operantion_dict["operator"] == "*":
-    produto = interactive_multiplication(operantion_dict["operand"])
-    print("Produto = {}".format(produto))
+    elif argv[1] == "-s":
+        try:
+            operation = []
+            for x in argv[2:]:
+                operation.append(int(x))
+            difference = subtraction(operation)
+            print("Diferença = {}".format(difference))
+        except:
+            print("Atenção! Passe como parâmetro, números separados por espaço.")
+            print("Ex.: -a 10 10 retornará o valor 20")
 
-elif operantion_dict["operator"] == "/":
-    quotient = interactive_division(operantion_dict["operand"])
-    print("Quociente = {}".format(quotient))
+    elif argv[1] == "-m":
+        operation = []
+        for x in argv[2:]:
+            operation.append(int(x))
+        produto = multiplication(operation)
+        print("Produto = {}".format(produto))
+    
+    elif argv[1] == "-d":
+        operation = []
+        for x in argv[2:]:
+            operation.append(int(x))
+        quotient = division(operation)
+        print("Quociente = {}".format(quotient))
+
+else:
+    interaction_data = input("Digite uma operação matemática: ")
+    operation_dict = operator_verification(interaction_data)
+    if operation_dict["operator"] == "+":
+        sum = addition(operation_dict["operand"])
+        print("Soma = {}".format(sum))
+
+    elif operation_dict["operator"] == "-":
+        difference = subtraction(operation_dict["operand"])
+        print("Diferença = {}".format(difference))
+
+    elif operation_dict["operator"] == "*":
+        produto = multiplication(operation_dict["operand"])
+        print("Produto = {}".format(produto))
+
+    elif operation_dict["operator"] == "/":
+        quotient = division(operation_dict["operand"])
+        print("Quociente = {}".format(quotient))
--- operation.py
+++ operation.py
@@ -1,12 +1,23 @@
+"""
+    Neste arquivo estão todas as funções necessárias para
+    as operações matemáticas suportadas pelo CLI_CALCULATOR.
+"""
+
 
 def operator_verification(data_str):
+    """
+        Essa função recebe como parâmetro uma string que contêm a
+        operação desejada pelo usuário. Uma string recebida no formato
+        ex: 10 + 12 + 13. A função separa os operadores dos operando e retorna
+        um dictionary contendo os dados separados.
+    """
     if " " in data_str:
         operation_data = data_str.split()
         operand = []
         for iterate in operation_data:
             if iterate.isnumeric():
                 operand.append(iterate)
-            elif iterate == "+" or iterate == "-" or iterate == "*" or iterate == "/" or iterate == "^":
+            elif (iterate == "+") or (iterate == "-") or (iterate == "*") or (iterate == "/") or (iterate == "^"):
                 operator = iterate
         operation_dict = {
             "operand": operand,
@@ -14,27 +25,28 @@ def operator_verification(data_str):
         }
         return operation_dict
 
-def interactive_addition(operand):
+
+def addition(operand):
     sum = 0
     for x in operand:
         sum += int(x)
     return sum
 
-def interactive_subtraction(operand):
+def subtraction(operand):
     difference = int(operand[0])
     operand = list(operand[1:])
     for x in operand:
         difference = difference - int(x)
     return difference
 
-def interactive_multiplication(operand):
+def multiplication(operand):
     product = int(operand[0])
     operand = list(operand[1:])
     for x in operand:
         product = product * int(x)
     return product
 
-def interactive_division(operand):
+def division(operand):
     quotient = int(operand[0])
     operand = list(operand[1:])
     for x in operand:
